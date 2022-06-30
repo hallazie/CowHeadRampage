a9bcaf21ec958a7e5cb3a7e41d9e299cdfb048ba
hallazie <hallazie@outlook.com>
2022-07-01 00:35:23 +0800

    add wolfhead basic move

--- Scripts/Characters/WolfHead/WolfHeadController.cs
+++ Scripts/Characters/WolfHead/WolfHeadController.cs
@@ -4,16 +4,35 @@ using UnityEngine;
 
 public class WolfHeadController : MonoBehaviour
 {
+
+    public GameObject cowHead;
+    public Animator animator;
+
+    public float runSpeed = 1f;
+    public float visionRange = 3f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        cowHead = GameObject.Find("CowHead");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 distance = cowHead.transform.position - gameObject.transform.position;
+        if (distance.magnitude < visionRange)
+        {
+            distance = distance.normalized;
+            Vector3 target = new Vector3(distance.x * runSpeed * Time.deltaTime, distance.y * runSpeed * Time.deltaTime, 0);
+            transform.up = target.normalized;
+            transform.position += target;            
+            animator.SetFloat("Speed", 1f);
+        }
+        else
+        {
+            animator.SetFloat("Speed", 0);
+        }
     }
 
 
